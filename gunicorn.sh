72c736124d222d52e6662b06f4dd9f9d87de27c9
gsuresh <gsureshb89@gmail.com>
2022-10-23 23:18:50 +0530

    added gunicorn config

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,4 +2,27 @@
 
 source env/bin/activate
 
+cd /var/lib/jenkins/workspace/django-cicd
+
+python3 manage.py makemigrations
+
+python3 manage.py migrate
+
+echo "Migrations done"
+
+cd /var/lib/jenkins/workspace/django-cicd
+
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "$USER"
+echo "$PWD"
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+echo "gunicorn has been started"
+sudo systemctl status gunicorn
+sudo systemctl restart gunicorn
 
