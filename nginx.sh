4b3a658be4f8ad12b40ffffd570cbb45c131e3cc
gsuresh <gsureshb89@gmail.com>
2022-10-24 19:48:30 +0530

    err

--- nginx.sh
+++ nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo cp -rf herodashboard_server.conf /etc/nginx/conf.d
 chmod 710 /var/lib/jenkins/workspace/django-cicd
-sudo ln -s /etc/nginx/sites-available/ /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
